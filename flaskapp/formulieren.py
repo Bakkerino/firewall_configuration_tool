d2cea8d41b3e4c19c99dc6b4d978fa89d4ba8b5d
Bakkerino <k.m.bakker@st.hanze.nl>
2020-05-07 09:27:01 +0200

    Registratie formulier

--- flaskapp/formulieren.py
+++ flaskapp/formulieren.py
@@ -1,22 +1,24 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo #validatiemiddelen voor inputvelden
 
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Gebruikersnaam',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email',
-                        validators=[DataRequired(), Email()])
-    password = PasswordField('Wachtwoord', validators=[DataRequired()])
-    confirm_password = PasswordField('Wachtwoord bevestigen',
-                                     validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Registreren')
-
-
-class LoginForm(FlaskForm):
+## Login input velden
+class LoginFormulier(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Wachtwoord', validators=[DataRequired()])
     remember = BooleanField('Onthouden')
     submit = SubmitField('Login')
+
+## Registratie input velden
+class RegistratieFormulier(FlaskForm):
+    username = StringField('Gebruikersnaam',
+        validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+    password = PasswordField('Wachtwoord', 
+        validators=[DataRequired()])
+    confirm_password = PasswordField('Wachtwoord bevestigen',
+        validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Registreren')
+

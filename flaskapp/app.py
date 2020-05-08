dd7e60fd1ec0e86cdc2e2ea547a1ff8157dd4995
Bakkerino <k.m.bakker@st.hanze.nl>
2020-05-08 16:19:21 +0200

    foutoplossingen, gebruikers en sessies

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect, flash
+from flask import Flask, render_template, url_for, request, redirect, flash, session
 from random import random, randint
 from formulieren import RegistratieFormulier, LoginFormulier
 app = Flask(__name__)
@@ -7,12 +7,12 @@ app.config['SECRET_KEY'] = 'dc7c6759cbb3d6ce0d57d790ec3b8ffb'
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
-
-#@app.route("/<usr>")
-#def user(usr):
-#    return f"{usr}"
-
+    if "user" in session:
+        user = session["user"]
+        return render_template("home.html", user=user)
+        #return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -25,15 +25,26 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = LoginFormulier()
-    if form.validate_on_submit():
-        if form.username.data == 'admin' and form.password.data == 'P@ssword':
-            flash(f'Je bent ingelogd als {form.username.data}!', 'success')
-            return redirect(url_for('home'))
-        else:
-            flash('Inloggen niet gelukt, probeer het overnieuw.', 'danger')
-    return render_template('login.html', title='Login', form=form)
+    if "user" in session:
+        user = session["user"]
+        flash(f'U bent ingelogd als {user}', 'success')
+        return redirect(url_for("home"))
+    else:
+        form = LoginFormulier()
+        if form.validate_on_submit():
+            if form.username.data == 'admin' and form.password.data == 'P@ssword':
+                session["user"] = form.username.data
+                flash(f'Je bent ingelogd als {form.username.data}!', 'success')
+                return redirect(url_for('home'))
+            else:
+                flash('Inloggen niet gelukt, probeer het overnieuw.', 'danger')
+        return render_template('login.html', title='Login', form=form)
 
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    flash(f'Uitgelogd', 'success')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)

2f776e8717f905f8dca3468104e2fa9cdfcc432e
Bakkerino <k.m.bakker@st.hanze.nl>
2020-06-05 16:47:33 +0200

    concept parser toegevoegd aan webapplicatie

--- configuratiewebapp/configuratietool/generator.py
+++ configuratiewebapp/configuratietool/generator.py
@@ -1,5 +1,7 @@
+import json
 
 outputData = []
+config = {}
 
 def genFortianalyzer(server, serial):
     outputData.append((f"""config log fortianalyzer setting
@@ -11,12 +13,33 @@ end\n"""))
     return outputData
 
 def inputBestandVerwerking(bestand):
-    verwerktbestand = "### VERWERKING ###" + "\n\n"
-    for line in bestand.splitlines():
+    #verwerktbestand = "### VERWERKING ###" + "\n\n"
+    #for line in bestand.splitlines():
         ## Hier kan het bestand geanalyseerd worden per regellijn
-        
+    for line in bestand.splitlines():
+        print(line.split())
+        if line == "" or line[0] == "#":
+            print("leeg of comment")
+            continue
+        args = line.split()
+        action, *args = line.split()
 
-        verwerktbestand += (line + "\n") 
+        if action == 'config':
+            header  = ' '.join(args)
+            if header not in config:
+                config[header] = {}
+
+        if action == 'edit':
+            section = ' '.join(args).strip('"')
+            if section not in config[header]:
+                config[header][section] = {}
+
+        if action == 'set':
+            name  = args.pop(0)
+            value = ' '.join(args).strip('"')
+            config[header][section][name] = value
+    #verwerktbestand += (line + "\n") 
     else:
         pass
+    verwerktbestand = json.dumps(config, indent=4)    
     return verwerktbestand

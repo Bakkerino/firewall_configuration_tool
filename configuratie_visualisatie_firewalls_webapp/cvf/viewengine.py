10fb6e79543808ea7ddf8cf1b67e827102b43f99
Bakkerino <k.m.bakker@st.hanze.nl>
2020-06-18 15:30:01 +0200

    verdere visualisatie, en kleuren

--- configuratie_visualisatie_firewalls_webapp/cvf/viewengine.py
+++ configuratie_visualisatie_firewalls_webapp/cvf/viewengine.py
@@ -19,14 +19,14 @@ def genConfigToTableHTML(jsonConfigObject):
     html += "</tbody>" + "</table>"
     return html
 
-def genConfigToAccordeon(jsonConfigObject, arguments, border_color):
+def genConfigToAccordeon(jsonConfigObject, arguments, gradeColor):
     html = ""
     for header, sectionData in jsonConfigObject.items():
 
         if arguments == True or header in arguments:
             header = header.lower().replace(' ', '-')
             html += "<div class=\"accordion\" id=\"accordion" + header + "\"><div class=\"card\"><div class=\"card-header\" id=\"heading" + header + "\">"
-            html += "<h2 class=\"mb-0\"><button class=\"btn btn-link btn-block text-left collapsed\" id=\"accordeonbutton\" type=\"button\" style=\"color: " + border_color + "\"data-toggle=\"collapse\" data-target=\"#collapse" + header + "\" aria-expanded=\"true\" aria-controls=\"collapse" + header + "\">Specifieke instellingen (" + header + ")</button></h2></div>"
+            html += "<h2 class=\"mb-0\"><button class=\"btn btn-link btn-block text-left collapsed\" id=\"accordeonbutton\" type=\"button\" style=\"color: " + gradeColor + "\"data-toggle=\"collapse\" data-target=\"#collapse" + header + "\" aria-expanded=\"true\" aria-controls=\"collapse" + header + "\">Specifieke instellingen (" + header + ")</button></h2></div>"
             html += "<div id=\"collapse" + header + "\" class=\"collapse\" aria-labelledby=\"heading" + header + "\" data-parent=\"#accordion" + header + "\">"
             html += "<div class=\"card-body\">"
             html += "<td class=\"hide\" style=\"display: none;\" id=\"dataview\">" + "<table id=\"viewtable\" class=\"table table-sm borderless\">" + "<tbody>"
@@ -39,38 +39,56 @@ def genConfigToAccordeon(jsonConfigObject, arguments, border_color):
             html += "</div></div></div>"
     return html
 
-def genCardMenus(jsonConfigObject, border_color="warning"):
+def genCardMenus(jsonConfigObject, gradeColor="warning"):
+
     html = genHeadOveriew(jsonConfigObject) # header
     
-    arguments = True #['interface']
+    arguments = True #['interface', 'firewall policy']
     for header, sectionData in jsonConfigObject.items():
         if header in ['config', 'global']:
             continue
         if arguments == True or header in arguments:
-            html += "<header class=\"p-3 mb-2 bg-grey rounded border border-" + border_color + "\" id=\"" + header.replace(' ', '-') + "\">"
+            print(header)
+            gradeColor, content, logo = genContentCard(header, sectionData)
+            headerInfo = getPopoverContents(header)
+            html += "<header class=\"p-3 mb-2 bg-grey rounded border border-" + gradeColor + "\" id=\"" + header.replace(' ', '-') + "\">"
             html += "<div class=\"media\">"
-            html += "<img src=\"static/icons/fortigate.png\" class=\"align-self-center mr-3\" alt=\"" + header.replace(' ', '-') + "\">"
+            html += "<img src=\"static/icons/" + logo + "\" class=\"align-self-start mr-3\" alt=\"" + header.replace(' ', '-') + "\">"
             html += "<div class=\"media-body\" id=\"mediaoverzicht\">"
-            html += "<h5 class=\"mt-0\"><b>" + header.capitalize() + "</b></h5>"
-            html += "<p>tekst of tabellen</p>"
+            html += "<h5 class=\"mt-0\"><b>" + header.capitalize() + "</b> "
+            html += """<button type=\"button\" data-trigger="focus" class="btn btn-sm btn-""" + headerInfo[2] + """\" data-toggle=\"popover\" 
+                    title=\"""" +  headerInfo[0] + """\" data-content=\"""" + headerInfo[1] + "\">Info</button></h5>"
+            html += "<legend class=\"border-bottom mb-1\"></legend>"
+            html += "<p>" + content + "</p>"
             html += "</div></div>"
-            html += genConfigToAccordeon(jsonConfigObject, [header], border_color)
-            html += "</header>"
+            html += genConfigToAccordeon(jsonConfigObject, [header], boostrapColorToCSSColor(gradeColor))
+            html += "</header>"   
     return html
 
-def boostrapColorToCSSColor(color):
-    if color == "success": color = "green"
-    if color == "warning": color = "yellow"
-    if color == "danger": color = "red"
-    return color
+def genContentCard(header, sectionData):
+    html = ""; logo = "fortigate.png"; gradeColor = "warning"; 
+
+    if header == 'firewall policy':
+        logo = "firewallpolicy.png"
+        pos= "front"
+        for section, valueData in sectionData.items():
+            html += "<h5>" + valueData['name'] + "</h5>"
+            html += "<th>" + "service" + "</th>" + "<td>" + valueData.get('service', 'none') + "</td>"
+
+    if header == 'interface':
+        logo = "interface5.png"
+        for section, valueData in sectionData.items():
+            html += "<h5>" + section + "(" + valueData.get('alias', '') + ")" + "</h5>"
+    return gradeColor, html, logo
+
 
 def genHeadOveriew(cfgJsonObject):
     html = ""
     if cfgJsonObject.get('config', False) and cfgJsonObject.get('global', False):
-        firewallversion = getSecurityGrade(cfgJsonObject['config']['version']['version'].split('-')[1])
+        firewallversion = getPopoverContents(cfgJsonObject['config']['version']['version'].split('-')[1])
         html += """<header class=\"p-3 mb-2 bg-grey rounded border border-""" + firewallversion[2] + """\" id=\"overzichtheader\" >
               <div class=\"media\">
-              <img src=\"static/icons/fortigate.png\" class=\"align-self-center mr-3\" alt=\"fortigate\">
+              <img src=\"static/icons/fortigate.png\" class=\"align-self-start mr-3\" alt=\"fortigate\">
               <div class=\"media-body\" id=\"mediaoverzicht\">
                 <h5 class=\"mt-0\"><b>""" + cfgJsonObject['config']['version']['version'].split('-')[0].replace('FGT', 'FortiGate ') + """</b></h5>
 
@@ -82,7 +100,7 @@ def genHeadOveriew(cfgJsonObject):
                     </tr>
                     <tr>
                       <th>Software versie:</th><td><button type=\"button\" class="btn btn-sm btn-""" + firewallversion[2] +  """\" data-toggle=\"popover\" 
-                      title=\"""" +  firewallversion[0] + """\" data-content=\"""" + firewallversion[1] + """\">
+                      title=\"""" +  firewallversion[0] + """\" data-trigger="focus" data-content=\"""" + firewallversion[1] + """\">
                         """ + cfgJsonObject['config']['version']['version'].split('-')[1] + """</button></td>
                       <th>Build:</th> <td>""" + cfgJsonObject['config']['version']['build'] + """</td>
                     </tr>
@@ -96,7 +114,7 @@ def genHeadOveriew(cfgJsonObject):
     else:
         return html
 
-def getSecurityGrade(arg):
+def getPopoverContents(arg):
     f = open('./cvf/reference.json',)
     reference = json.load(f)
     if reference.get(arg, False):
@@ -110,6 +128,13 @@ def getSecurityGrade(arg):
         color = "warning"
         return title, content, color
 
+
+def boostrapColorToCSSColor(color):
+    if color == "success": color = "green"
+    if color == "warning": color = "#ffbf00"
+    if color == "danger": color = "red"
+    if color == "info": color = "blue"
+    return color
 
 
   
